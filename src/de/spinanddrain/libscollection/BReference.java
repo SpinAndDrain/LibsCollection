046be56e3bb204ef5c8e34751a2e01da9cfa69d4
SpinAndDrain <unconfigured@null.spigotmc.org>
2020-06-19 10:35:29 +0200

    Updated to v1.2.1

--- src/de/spinanddrain/libscollection/BReference.java
+++ src/de/spinanddrain/libscollection/BReference.java
@@ -1,33 +1,61 @@
 package de.spinanddrain.libscollection;
 
+import java.io.File;
+import java.io.IOException;
+
 import de.spinanddrain.logging.Log;
 import de.spinanddrain.logging.LogType;
 import de.spinanddrain.updater.Updater;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 
 public class BReference extends Plugin {
 	
 	protected static BReference instance;
 	private Log log;
 	
+	private boolean check;
+	
 	@Override
 	public void onEnable() {
 		instance = this;
 		log = new Log(getProxy().getConsole(), getProxy().getLogger(), LogType.RAW);
-		Updater u = Libraries.getPluginUpdaterFor(getDescription().getVersion());
-		log.log("§7[§6LibsCollection§7] §eChecking §efor §eupdates...");
 		try {
-			if(u.isAvailable())
-				log.log("§7[§6LibsCollection§7] §eA §enewer §eversion §eis §eavailable: §b" + u.getLatestVersion());
-			else
-				log.log("§7[§6LibsCollection§7] §eNo §eupdates §efound. §eYou §eare §erunning §ethe §elatest §eversion.");
-		} catch(Exception e) {
-			log.log("§7[§6LibsCollection§7] §cAn §cerror §coccurred §c(" + e.getMessage() + ")");
+			io();
+		} catch (IOException e1) {
+			log.log("§7[§6LibsCollection§7] §cAn §cerror §coccurred §c(" + e1.getMessage() + ")");
+		}
+		if(check) {
+			Updater u = Libraries.getPluginUpdaterFor(getDescription().getVersion());
+			log.log("§7[§6LibsCollection§7] §eChecking §efor §eupdates...");
+			try {
+				if(u.isAvailable())
+					log.log("§7[§6LibsCollection§7] §eA §enewer §eversion §eis §eavailable: §b" + u.getLatestVersion());
+				else
+					log.log("§7[§6LibsCollection§7] §eNo §eupdates §efound. §eYou §eare §erunning §ethe §elatest §eversion.");
+			} catch(Exception e) {
+				log.log("§7[§6LibsCollection§7] §cAn §cerror §coccurred §c(" + e.getMessage() + ")");
+			}
 		}
 	}
 
 	public Log getLog() {
 		return log;
+	}
+	
+	private void io() throws IOException {
+		File f = new File("plugins/LibsCollection/config.yml");
+		if(!f.getParentFile().exists())
+			f.getParentFile().mkdirs();
+		f.createNewFile();
+		Configuration cfg = ConfigurationProvider.getProvider(YamlConfiguration.class).load(f);
+		if(!cfg.contains("updater")) {
+			cfg.set("updater", true);
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(cfg, f);
+		}
+		this.check = cfg.getBoolean("updater");
 	}
 	
 }
--- src/de/spinanddrain/libscollection/SReference.java
+++ src/de/spinanddrain/libscollection/SReference.java
@@ -1,5 +1,9 @@
 package de.spinanddrain.libscollection;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.spinanddrain.logging.Log;
@@ -11,24 +15,45 @@ public class SReference extends JavaPlugin {
 	protected static SReference instance;
 	private Log log;
 	
+	private boolean check;
+	
 	@Override
 	public void onEnable() {
 		instance = this;
 		log = new Log(getServer().getConsoleSender(), getServer().getLogger(), LogType.RAW);
-		Updater u = Libraries.getPluginUpdaterFor(getDescription().getVersion());
-		log.log("§7[§6LibsCollection§7] §eChecking for updates...");
 		try {
-			if(u.isAvailable())
-				log.log("§7[§6LibsCollection§7] §eA newer version is available: §b" + u.getLatestVersion());
-			else
-				log.log("§7[§6LibsCollection§7] §eNo updates found. You are running the latest version.");
-		} catch(Exception e) {
-			log.log("§7[§6LibsCollection§7] §cAn error occurred (" + e.getMessage() + ")");
+			io();
+		} catch (IOException e1) {
+			log.log("§7[§6LibsCollection§7] §cAn error occurred (" + e1.getMessage() + ")");
+		}
+		if(check) {
+			Updater u = Libraries.getPluginUpdaterFor(getDescription().getVersion());
+			log.log("§7[§6LibsCollection§7] §eChecking for updates...");
+			try {
+				if(u.isAvailable())
+					log.log("§7[§6LibsCollection§7] §eA newer version is available: §b" + u.getLatestVersion());
+				else
+					log.log("§7[§6LibsCollection§7] §eNo updates found. You are running the latest version.");
+			} catch(Exception e) {
+				log.log("§7[§6LibsCollection§7] §cAn error occurred (" + e.getMessage() + ")");
+			}
 		}
 	}
 
 	public Log getLog() {
 		return log;
+	}
+	
+	private void io() throws IOException {
+		File f = new File("plugins/LibsCollection/config.yml");
+		if(!f.getParentFile().exists())
+			f.getParentFile().mkdirs();
+		f.createNewFile();
+		YamlConfiguration cfg = YamlConfiguration.loadConfiguration(f);
+		cfg.options().copyDefaults(true);
+		cfg.addDefault("updater", true);
+		cfg.save(f);
+		this.check = cfg.getBoolean("updater");
 	}
 	
 }
